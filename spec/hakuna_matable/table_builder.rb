b8cdaab5eb20df7860c1e68d2ad882d1bca94524
Vitor HP <vitorhp2@gmail.com>
2014-02-25 01:13:58 -0300

    Removed unused files and updated gitignore

--- spec/hakuna_matable/table_builder.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
-describe "HakunaMatable::TableBuilder" do
-  describe "#table_for" do
-    it "renders a table for a collection" do
-      fail "Need fixtures for this to word properly"
-    end
-  end
-end
